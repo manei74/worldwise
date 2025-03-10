0e2bc8bdda37ac231a344b15b61c8e97c88e00b2
Nikita Maneev <nick@Nikitas-iMac.local>
2025-03-10 13:45:58 -0600

    Updates

---
+++ assets/Logo-2e1ec2d2.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-af2e459d.js";const l="_logo_7b2ls_1",t={logo:l};function g(){return o.jsx(s,{to:"/",children:o.jsx("img",{src:"/worldwise/logo.png",alt:"WorldWise logo",className:t.logo})})}export{g as L};
