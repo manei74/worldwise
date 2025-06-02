aaee39928d789e08fabbb019813ff37f18d199e5
Nikita Maneev <nick@Nikitas-iMac.local>
2025-06-02 14:10:25 -0600

    Updates

--- assets/Logo-4f6a2bce.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as s}from"./index-e2b501d5.js";const l="_logo_7b2ls_1",t={logo:l};function g(){return o.jsx(s,{to:"/",children:o.jsx("img",{src:"/worldwise/logo.png",alt:"WorldWise logo",className:t.logo})})}export{g as L};
--- assets/PageNotFound-3b52f192.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-e2b501d5.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
